b7271038470c8f1f3afb59d41e3f9af3314b7bbe
Beytullah Çakır <132066307+beytullah-cakir@users.noreply.github.com>
2026-08-12 14:03:10 +0300

    added necessary tables

---
+++ database/migrations/2026_08_12_105456_create_click_logs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('click_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("url_id")->constrained("urls")->onDelete("cascade");
+            $table->string("ip_address",45)->nullable();
+            $table->text("user_agent")->nullable();
+            $table->string("referer",2048)->nullable();
+            $table->timestamp('visited_at')->useCurrent();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('click_logs');
+    }
+};
